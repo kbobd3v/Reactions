6cc3fc58fa0350df50d15ae038e66edad5d01010
kbdev <leonard@airp.ws>
2022-02-04 20:40:46 -0500

    reviews project: finished

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -6,6 +6,38 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   // use index to go through the objects array
   const {name, job, image, text } = people[index];
+
+const checkNumber = (number) => {
+  if (number > people.length -1){
+    return 0;;
+  }
+  if (number < 0){
+    return people.length -1;
+  }
+  return number;
+};
+
+  const nextPerson = () => {
+    setIndex((index) =>{
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) =>{
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index){
+        randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  }
   return (<article className='review'>
     <div className='img-container'>
         <img src={image} alt={name} className='person-img'/>
@@ -17,16 +49,16 @@ const Review = () => {
     <p className='job'>{job}</p>
     <p className='info'>{text}</p>
     <div className='button-container'>
-      <button className='prev-btn'>
+      <button className='prev-btn' onClick={prevPerson}>
         <FaChevronLeft />
       </button>
-      <button className='next-btn'>
+      <button className='next-btn' onClick={nextPerson}>
         <FaChevronRight />
       </button>
-      <button className='random-btn'>
+    </div>
+    <button className='random-btn' onClick={randomPerson}>
         Surprise me
       </button>
-    </div>
   </article>
   )
 };
