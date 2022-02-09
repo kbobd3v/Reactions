27b3c9f622e226daac74eecec59301e268bb11f0
Kbob <leonard@airp.ws>
2022-02-08 23:58:01 -0500

    lorem ipsum project: UI fix done

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    // This allows you to cancel the action in order to send it several times
+    e.preventDefault();
+    setText(data);
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>Tired of bored lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      {/* htmlFor returns the value of the label */}
+      <label htmlFor='amount'>
+        paragraphs:
+      </label>
+      <input type="number" name="amount" id='amount' value={count}
+      // onChange search for changes in values
+      // So we hear the event, look for the value in its target
+      onChange={(e) => { setCount(e.target.value)}} />
+      <button className='btn' type='submit'>Paragraphs</button>
+    </form>
+    <article className='lorem-text'>
+      {text.map((item, index) => {
+        return <p key={index}>{item}</p>
+      })}
+    </article>
+  </section>
     )
 }
 
