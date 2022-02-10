9eab1aec7bc1783ab4ce4c2efafebc5de77a778b
kbdev <leonard@airp.ws>
2022-02-10 13:01:22 -0500

    grocery bud: UI finished

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -3,7 +3,41 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName]= useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: ''});
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  }
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {/* remember, use && for check only one condition */}
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input 
+          type='text' 
+          className='grocery' 
+          placeholder='e. g. egss' 
+          value={name}
+          onChange={(e) => setName=e.target.value} />
+          <button type='submit' className='submit-btn'>
+            {/* but if you are checking if-else conditions use ? and :*/}
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear list</button>
+      </div>
+    </section>
+  )
 }
 
 export default App
