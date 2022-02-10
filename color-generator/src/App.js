f2ee109170b4bf559c5616be6be9628fe206b1e1
kbdev <leonard@airp.ws>
2022-02-10 12:09:04 -0500

    color generator: final comments

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -6,6 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
+  // Asign a default value on the list
   const [list, setList] = useState(new Values('#F15025').all(10));
 
   const handleSubmit = (e) => {
