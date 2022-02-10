e10fc1fd6fb689e0b475ecbc3689269c77503680
kbdev <leonard@airp.ws>
2022-02-09 20:09:28 -0500

    color generator: project finished

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -4,7 +4,48 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#F15025').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      // Values class allows us get different color values from our initial state got it from input
+      // it returns an array of objects
+      let colors = new Values(color).all(10);
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(true)
+      console.log(error);
+    }
+  }
+  return (
+    // This is a react fragment label, use it when you need to render several labels un one function return
+    <>
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type='text'
+         value={color}
+         onChange={(e) => setColor(e.target.value)}
+         placeholder="#f15025"
+         // if error useState is true then add className
+         className={`${error ? 'error' : null}`} />
+        <button className='btn' type="submit">
+          submit
+        </button>
+      </form>
+    </section>
+    <section className='colors'>
+      {list.map((color, index) => {
+        console.log(color);
+        return <SingleColor key={index} {...color} index={index} hex={color.hex} />
+      })}
+    </section>
+    </>
+  )
 }
 
 export default App
--- color-generator/src/SingleColor.js
+++ color-generator/src/SingleColor.js
@@ -1,8 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index, hex }) => {
+  const [alert, setAlert] = useState(false);
+  // We use join because rbg return an array and we want join those array values into a string divided by coma
+  const bgc = rgb.join(',');
+  // use template string to attach # to the hex variable
+  const hexValue = `#${hex}`;
+  // everytime alert value change we'll set a timeout to change the value again to false
+  // in order to make the clipboard text disappear after 3 seconds or 3000 miliseconds
+  useEffect(() => {
+    const timeout = setTimeout(() =>{
+      setAlert(false);
+    }, 3000);
+    // after the setTimeout ran, clear the whole timeout (this prevents the overload of timeouts)
+    return () => clearTimeout(timeout);
+  }, [alert]);
+  return (
+    // We add templates strings on the classname, because we want to check
+    // if the index (which is also the number or base where the color makes hard to read info) 
+    // is above 10, then change the weight of text color within the container
+    //  when adding in-line style templates remember camelCase for properties and : to divide properties from values
+    // look we use rgb on template strings ``, we'll pass the bgc joined string
+    // to use it as a color value for background-color property
+    <article 
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{backgroundColor: `rgb(${bgc})`}}
+      onClick={() => {
+        setAlert(true);
+        // Take the navigator's clipboard element and write on it the value you want
+        // in this case we'll pass hexValue
+        navigator.clipboard.writeText(hexValue);
+        }}>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {/* If alert state value is true, display this paragraph */}
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
