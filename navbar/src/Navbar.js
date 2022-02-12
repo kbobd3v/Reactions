475c44709a777c139b5875e5c0ee98d17d964543
Kbob <leonard@airp.ws>
2022-02-12 08:58:37 -0500

    navbar project: finished

--- navbar/src/Navbar.js
+++ navbar/src/Navbar.js
@@ -5,7 +5,19 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
+  // we use this effect to control the height of the links-container
+  // useRef allows us access  DOM elements after render
+  useEffect(()=> {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
   return <nav>
     <div className='nav-center'>
       <div className='nav-header'>
@@ -14,8 +26,8 @@ const Navbar = () => {
         <FaBars />
       </button>
       </div>
-        <div className='links-container'>
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
               const {id, url, text}= link;
               return (<li key={id}>
