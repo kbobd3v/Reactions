50de06b89ee79eb4bfd43910bc009e0b30ff4c2b
kbdev <leonard@airp.ws>
2022-02-14 21:02:25 -0500

    sidebar project: finished

--- sidebar-modal/src/context.js
+++ sidebar-modal/src/context.js
@@ -3,11 +3,39 @@ import React, { useState, useContext } from 'react'
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
-    return <AppContext.Provider value='holas'>
+    // Let's set our global state value variables
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
+
+    return <AppContext.Provider value={{
+        isModalOpen,
+        isSidebarOpen,
+        openSidebar,
+        openModal,
+        closeModal, 
+        closeSidebar
+    }}>
         {children}
     </AppContext.Provider>
 }
 
 // custom hook
+// Remember when naming hooks always start with 'use'
+export const useGlobalContext= () => {
+    return useContext(AppContext);
+}
 
-export { AppContext, AppProvider} 
+export { AppContext, AppProvider } 
