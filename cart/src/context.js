7fd07b1b0409e85350e9141a749107ea31f0a138
Kbob <leonard@airp.ws>
2022-02-16 23:53:59 -0500

    cart project: finished clear cart fix, remove item, increase, decrease

--- cart/src/context.js
+++ cart/src/context.js
@@ -6,13 +6,46 @@ import reducer from './reducer'
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
+const initialState = {
+  loading:false,
+  cart:cartItems,
+  total:0,
+  amount:0,
+}
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  // useReduce will help us to manage our many states values
+  // it recieves state which will be our initial value
+  // and dispatch, this will help us shooting our change of state
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    // dispatch recieves and object with type, this will
+    // tell us which option on the reducer we want to run
+    dispatch({ type: 'CLEAR_CART'})
+  }
+
+  const remove = (id) => {
+    dispatch({ type: 'REMOVE', payload: id })
+  }
+
+  const increase = (id) => {
+    dispatch({type: 'INCREASE', payload: id })
+  }
+
+  const decrease = (id) => {
+    dispatch({type: 'DECREASE', payload: id })
+  }
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        // sending ...state, we use spread operator to send several states
+        ...state,
+        clearCart,
+        remove, 
+        increase,
+        decrease,
       }}
     >
       {children}
---
+++ cart/src/reducer.js
@@ -0,0 +1,38 @@
+const reducer = (state, action) => {
+    if (action.type === 'CLEAR_CART') {
+        // always need to pass previous state with ... operator
+        // because we need our state remains intact but the part
+        // we're changing
+        return { ...state, cart: [] }
+    }
+
+    if (action.type === 'REMOVE') {
+        return {
+            ...state,
+            cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),}
+    }
+
+    if (action.type === 'INCREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload){
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+            return cartItem
+        });
+        return {...state, cart: tempCart}
+    }
+
+    if (action.type === 'DECREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload){
+                return {...cartItem, amount: cartItem.amount - 1}
+            }
+            return cartItem
+        }).filter((cartItem=> cartItem.amount !== 0))
+        return {...state, cart: tempCart}
+    }
+
+    return state
+}
+
+export default reducer;
